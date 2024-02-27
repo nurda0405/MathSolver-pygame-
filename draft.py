cb92e3d25b6dbd5f8767fa8d212ac4224ec41961
Nurdaulet <nurda0405@gmail.com>
2024-02-27 15:53:06 +0500

    did remove event

--- draft.py
+++ draft.py
@@ -1,6 +1,38 @@
 import pygame
 
 pygame.init()
+class Cell(pygame.sprite.Sprite):
+    def __init__(self):
+        self.x = "achivable"
+array = [[0]]
+cell = Cell()
+array[0][0] = cell
+print(array[0][0].x)
 screen = pygame.display.set_mode((640,640))
-clock = pygame.time.Clock()
+rect = pygame.Surface((50,50))
+rect.fill((0,0,255))
+rectangle = rect.get_rect()
+screen.fill((255,255,255))
+screen.blit(rect, (0,0))
+
+font = pygame.font.Font('freesansbold.ttf', 14)
+position = font.render(str(0) + " "+str(0), True,(0,0,0))
+pos_rect = position.get_rect()
+pos_rect.center = (200,200)
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouseX, mouseY = pygame.mouse.get_pos()
+            screen.fill((255,255,255))
+            if 0 <= mouseX <= 50 and 0 <= mouseY <= 50:
+                rect.fill((255,0,0))
+            position = font.render(str(mouseX)+" "+str(mouseY), True,(0,0,0))
+            screen.blit(rect, (0,0))
+            screen.blit(position, pos_rect)
+            
+    pygame.display.update()
 
--- math_solver.py
+++ math_solver.py
@@ -1,12 +1,13 @@
 import random
-# fill the grid with numbers and figures, after that trace a path until the last cell.
+import pygame
 
+# fill the grid with numbers and figures, after that trace a path until the last cell.
 class Game:
     def find_path(self, x, y, path):
         #stop if reached the end cell
-        if (x == self.i - 1 and y == self.j - 2) or (x == self.i - 2 and y == self.j - 1):
+        if (x == self.row - 1 and y == self.col - 2) or (x == self.row - 2 and y == self.col - 1):
             self.reached = True
-            self.path = path + [[x,y], [7,7]]
+            self.path = path + [[x, y], [self.row - 1, self.col - 1]]
             return
         
         #shuffling the directions to avoid repeated paths
@@ -19,7 +20,7 @@ class Game:
                     i = d[0]
                     j = d[1]
                     if -1 < x + i < 8 and -1 < y + j < 8:
-                        if not [x+i, y+j] in path:
+                        if not [x + i, y + j] in path:
                             self.find_path(x + i, y + j, path + [[x, y]])
             #the reason why my previous program didn't worked correctly is that I had to not save the values in the variable within function as the path of wrong way interference with the traced path, instead, I had give the new value as a parameter of the next recursion function. Also, I had to know the working principle of the recursion call, for example, in the 'for' cycle the first called function executes until it reaches its end case and other called functions waits until the first is executed. For example, in my code I had to swap the order of if clause and for cycle to check whether the path reached the end cell, otherwise, it will continue working after reaching the end cell. 
     
@@ -28,7 +29,6 @@ class Game:
         figure = self.grid[x][y]
         i += 1
         x, y = self.path[i][0], self.path[i][1]
-        print(figure, self.grid[x][y])
 
         if figure == '+':
             sum += self.grid[x][y]
@@ -38,25 +38,25 @@ class Game:
         if i < self.length - 4: #why the last two elements are executed when I put -2, why does it work when I put -4?
             self.trace_path(self.path[i + 1][0], self.path[i + 1][1], i + 1, sum)
         else:
-            self.grid[7][7] = sum
+            self.grid[self.row - 1][self.col - 1] = sum
     
-    def __init__(self):
-        self.i = 8
-        self.j = 8
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
         self.path = []
         self.reached = False
-        self.grid = [[0 for i in range(self.i)] for j in range(self.j)]        
+        self.grid = [[0 for i in range(self.col)] for j in range(self.row)]        
         self.directions = [[0,1],[0,-1],[-1,0],[1,0]]        
         
                 
         #filling the grid with random values
-        for i in range(self.i):
-            for j in range(self.j):
+        for i in range(self.row):
+            for j in range(self.col):
                 if (i + j) % 2:    
                     self.grid[i][j] = random.choice(['+', '-'])
                 else:
                     self.grid[i][j] = random.randint(1,15)
-        self.grid[7][6], self.grid[6][7] = "=", "="
+        self.grid[self.row - 1][self.col - 2], self.grid[self.row - 2][self.col - 1] = "=", "="
 
         #finding a path until the last cell
         self.find_path(0, 0, [])
@@ -68,11 +68,79 @@ class Game:
         self.problem = ''
         for cell in self.path:
             self.problem += str(self.grid[cell[0]][cell[1]])
-
+class Cell(pygame.sprite.Sprite):
+    def __init__(self, gridX, gridY, screenX, screenY, value, width):
+        pygame.sprite.Sprite.__init__(self)
+        self.value = str(value)
+        self.width = width
+        self.gridX, self.gridY = gridX, gridY
+        self.screenX, self.screenY = screenX, screenY
+        self.rect = (self.screenX, self.screenY)
+        self.ticked = False
         
-         
-game = Game()
-for i in range(8):
-    print(game.grid[i],'\n')
-print(game.path,'\n')
-print(game.problem)
+        self.image = pygame.Surface((80,80))
+        self.image.fill((255,255,255))
+         #drawing the cell on the screen
+        self.text = font.render(self.value, True, (0,0,0))
+        self.textX, self.textY = self.screenX + self.width // 2 - 10, self.screenY + self.width // 2 - 10
+        self.draw()
+        
+    def click(self):
+        if not self.ticked:
+            self.image.fill((0,255,0))
+            player_path.append([self.gridX, self.gridY])
+            self.ticked = True
+            self.draw()
+
+    def remove(self):
+        self.ticked = False
+        self.image.fill((255,255,255))
+        self.draw()
+        print("removed!")
+
+    def draw(self):
+        screen.blit(self.image, (self.screenX, self.screenY))
+        screen.blit(self.text, (self.textX, self.textY))
+            
+
+pygame.init()
+screen = pygame.display.set_mode((730,730))
+font = pygame.font.Font('freesansbold.ttf',24)
+
+
+row = 8
+col = 8
+width = 80
+game = Game(8, 8)
+player_path = []
+cells = [[0 for i in range(col)] for j in range(row)]
+coordinates = [10 + 90 * i for i in range(row)]
+for i in range(row): #creating cells
+    for j in range(col):
+        cell = Cell(i, j, coordinates[i], coordinates[j], game.grid[i][j], width)
+        cells[i][j] = cell
+
+cells[0][0].click()
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouseX, mouseY = pygame.mouse.get_pos()
+            for i in range(row):
+                for j in range(col):
+                    if coordinates[i] <= mouseX <= coordinates[i] + width and coordinates[j] <= mouseY <= coordinates[j] + width: #i and j swaped?
+                        cells[i][j].click()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_z:
+                if len(player_path) > 1:
+                    gridX, gridY = player_path[-1][0], player_path[-1][1]
+                    player_path.pop()
+                    cells[gridX][gridY].remove()
+                    print(gridX, gridY, player_path)
+    pygame.display.update()
+
