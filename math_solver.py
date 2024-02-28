fb73d5343b2ce28e491b37fe56f9b8c791e805c6
Nurdaulet <nurda0405@gmail.com>
2024-02-28 16:28:25 +0500

    restart + show path + erase events done

--- math_solver.py
+++ math_solver.py
@@ -19,7 +19,7 @@ class Game:
                 if not self.reached:        
                     i = d[0]
                     j = d[1]
-                    if -1 < x + i < 8 and -1 < y + j < 8:
+                    if -1 < x + i < self.row and -1 < y + j < self.col:
                         if not [x + i, y + j] in path:
                             self.find_path(x + i, y + j, path + [[x, y]])
             #the reason why my previous program didn't worked correctly is that I had to not save the values in the variable within function as the path of wrong way interference with the traced path, instead, I had give the new value as a parameter of the next recursion function. Also, I had to know the working principle of the recursion call, for example, in the 'for' cycle the first called function executes until it reaches its end case and other called functions waits until the first is executed. For example, in my code I had to swap the order of if clause and for cycle to check whether the path reached the end cell, otherwise, it will continue working after reaching the end cell. 
@@ -40,7 +40,7 @@ class Game:
         else:
             self.grid[self.row - 1][self.col - 1] = sum
     
-    def __init__(self, row, col):
+    def __init__(self, row, col, min, max):
         self.row = row
         self.col = col
         self.path = []
@@ -55,7 +55,7 @@ class Game:
                 if (i + j) % 2:    
                     self.grid[i][j] = random.choice(['+', '-'])
                 else:
-                    self.grid[i][j] = random.randint(1,15)
+                    self.grid[i][j] = random.randint(min, max)
         self.grid[self.row - 1][self.col - 2], self.grid[self.row - 2][self.col - 1] = "=", "="
 
         #finding a path until the last cell
@@ -68,12 +68,13 @@ class Game:
         self.problem = ''
         for cell in self.path:
             self.problem += str(self.grid[cell[0]][cell[1]])
+
 class Cell(pygame.sprite.Sprite):
-    def __init__(self, gridX, gridY, screenX, screenY, value, width):
+    def __init__(self, gridX, gridY, screenY, screenX, value, width):
         pygame.sprite.Sprite.__init__(self)
         self.value = str(value)
         self.width = width
-        self.gridX, self.gridY = gridX, gridY
+        self.gridRow, self.gridCol = gridX, gridY
         self.screenX, self.screenY = screenX, screenY
         self.rect = (self.screenX, self.screenY)
         self.ticked = False
@@ -88,45 +89,74 @@ class Cell(pygame.sprite.Sprite):
     def click(self):
         if not self.ticked and self.isAdjacent():
             self.image.fill((0,255,0))
-            player_path.append([self.gridX, self.gridY])
+            player_path.append([self.gridRow, self.gridCol])
             self.ticked = True
             self.draw()
+            print(player_path)
+            if self.gridRow == row - 1 and self.gridCol == col - 1:
+                if player_path == game.path:
+                    print("You win!")
+                else:
+                    print("Try again...")
+                print('\n', player_path, '\n', game.path)
             
     def isAdjacent(self):
-        lastX, lastY = player_path[-1][0], player_path[-1][1]
-        if abs(lastX - self.gridX) <= 1 and abs(lastY - self.gridY) <= 1:
-            if not (abs(lastX - self.gridX) == 1 and abs(lastY - self.gridY) == 1):
+        lastRow, lastCol = player_path[-1][0], player_path[-1][1]
+        if abs(lastRow - self.gridRow) <= 1 and abs(lastCol - self.gridCol) <= 1:
+            if not (abs(lastRow - self.gridRow) == 1 and abs(lastCol - self.gridCol) == 1):
                 return True
         return False 
-    def remove(self):
-        self.ticked = False
-        self.image.fill((255,255,255))
-        self.draw()
 
     def draw(self):
         screen.blit(self.image, (self.screenX, self.screenY))
         screen.blit(self.text, (self.textX, self.textY))
-            
+    
+    def change_state(self):
+        if self.ticked:
+            self.image.fill((255,255,255))
+            self.ticked = False
+        else:
+            self.image.fill((0,255,0))
+            self.ticked = True
+        self.draw()
+
+def restart():
+    game.__init__(row, col, min, max)
+    for i in range(len(player_path) - 1):
+        player_path.pop()
+    for i in range(row): #creating cells
+        for j in range(col):
+            cell = Cell(i, j, coordinates[i], coordinates[j], game.grid[i][j], width)
+            cells[i][j] = cell
+    cells[0][0].click()
+    player_path.pop()
+    print(game.path)
+
+def erase():
+    while len(player_path) > 1:
+        cell= player_path.pop()
+        cells[cell[0]][cell[1]].change_state()
+
+def change_state(list_cells):
+    for cell in list_cells:
+        cells[cell[0]][cell[1]].change_state()
+
 pygame.init()
-screen = pygame.display.set_mode((730,730))
-font = pygame.font.Font('freesansbold.ttf',24)
-
-
 row = 8
 col = 8
 width = 80
-game = Game(8, 8)
-player_path = [[0, 0]]
-cells = [[0 for i in range(col)] for j in range(row)]
+min = 1
+max = 20
 coordinates = [10 + 90 * i for i in range(row)]
 
-for i in range(row): #creating cells
-    for j in range(col):
-        cell = Cell(i, j, coordinates[i], coordinates[j], game.grid[i][j], width)
-        cells[i][j] = cell
-        
-cells[0][0].click()
-player_path.pop()
+screen = pygame.display.set_mode((coordinates[row - 1] + width + 10, coordinates[col - 1] + width + 10))
+font = pygame.font.Font('freesansbold.ttf',24)
+
+game = Game(row, col, min, max)
+player_path = [[0, 0]]
+cells = [[0 for i in range(col)] for j in range(row)]
+restart()
+
 
 run = True
 
@@ -139,14 +169,32 @@ while run:
             mouseX, mouseY = pygame.mouse.get_pos()
             for i in range(row):
                 for j in range(col):
-                    if coordinates[i] <= mouseX <= coordinates[i] + width and coordinates[j] <= mouseY <= coordinates[j] + width: #i and j swaped?
+                    if coordinates[i] <= mouseY <= coordinates[i] + width and coordinates[j] <= mouseX <= coordinates[j] + width: #i and j swaped?
                         cells[i][j].click()
+                        print(i, j) #correct
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_z:
                 if len(player_path) > 1:
                     gridX, gridY = player_path[-1][0], player_path[-1][1]
                     player_path.pop()
-                    cells[gridX][gridY].remove()
+                    cells[gridX][gridY].change_state()
+            if event.key == pygame.K_F5:
+                restart()
+            if event.key == pygame.K_SPACE:
+                erase()
+            if event.key == pygame.K_KP_ENTER:
+                change_state(player_path)
+                change_state(game.path)
+                
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_KP_ENTER:
+                change_state(game.path)
+                change_state(player_path)
+
+
+                    
     pygame.display.update()
 
+#the error in restarting the game
+#the error in indexing of grid, when clicked the axes of the cell are swaped
